d68fd995b140ddf01ebe97a1502f74ba1320cebd
jmrideout <jmrideout@hotmail.com>
2020-11-28 00:13:01 -0600

    Added bitwise operator JS solution.

--- jason-solution.js
+++ jason-solution.js
@@ -1,11 +1,25 @@
-// Assumes always correct input
-const perm = (permission_string) => {
+// Assumes correct input
+// Uses reduce function to separate the array of letters,
+// which are converted into binary and placed into groups of 3.
+// The result is converted from binary to decimal and joined back into a string.
+const perm2 = (permission_string) => {
   return [...permission_string]
     .reduce((acc,cur,i)=>{
       acc[~~(i/3)] += cur == '-' ? '0':'1';
       return acc;
     } , new Array(3).fill(''))
     .map(n=>parseInt(n,2).toString())
+    .join('');
+}
+
+// Using bitwise operator
+const perm = (permission_string) => {
+  let digits = [...permission_string].map(n=>n==='-'?'0':'1');
+  groups = []
+  while (digits.length)
+    groups.push(digits.splice(0,3));
+  return groups
+    .map(e=>e.reduce((acc,cur,i)=>acc+(cur << e.length-i-1), 0))
     .join('');
 }
 
